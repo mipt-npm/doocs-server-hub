d5a7beac095f75aff3225e228fb208cd46deea8c
vasyoid <vasyoid@gmail.com>
2022-05-24 16:43:01 +0300

    ZMQ connection

--- src/main.cc
+++ src/main.cc
@@ -3,19 +3,65 @@
 
 #include <doocs_zmq.h>
 
-int main(int argc, char *argv[]) {
-    // TODO connect to zmq
-    std::shared_ptr<ZmqConnection> connection;
+std::vector<PropertyDescriptor> properties_from_json(const std::vector<nlohmann::json>& properties) {
+    std::vector<PropertyDescriptor> result;
+    result.reserve(properties.size());
 
-    // TODO ask for device properties via zmq
-    DeviceDescriptor desc = {
-        "EXAMPLE",
-        {
-            {"IR", "readonly property", DATA_INT, true, false},
-            {"IW", "writeonly property", DATA_INT, false, true},
-            {"IRW", "readwrite property", DATA_INT, true, true},
-        }
+    for (const auto& prop : properties) {
+        result.push_back({
+            prop["name"].get<std::string>(),
+            prop["info"].get<std::string>(),
+            prop["type"].get<int>(),
+            prop["readable"].get<bool>(),
+            prop["writable"].get<bool>()
+        });
+    }
+
+    return result;
+}
+
+DeviceDescriptor acquire_device_description(const std::shared_ptr<ZmqConnection>& connection) {
+    nlohmann::json acq = {
+        {"id", std::time(nullptr)},
+        {"origin", "doocs"},
+        {"format", "json"},
+        {"payload", { {"action", "names"} } }
     };
+
+    connection->push_socket.send(acq.dump());
+    zmqpp::poller poller;
+    poller.add(connection->sub_socket);
+
+    if (!poller.poll(1000)) {
+        throw std::runtime_error("could not acquire device description\n");
+    }
+
+    std::string response_string;
+    connection->push_socket.receive(response_string);
+
+    nlohmann::json response = nlohmann::json::parse(response_string)["payload"];
+
+    DeviceDescriptor desc = {
+        response["name"].get<std::string>(),
+        properties_from_json(response["properties"].get<std::vector<nlohmann::json>>())
+    };
+
+    return desc;
+}
+
+int main(int argc, char *argv[]) {
+    zmqpp::context zmq_ctx;
+    ZmqConnection zmq_connection = connect_to_zmq(zmq_ctx, argv[1]);
+    auto connection = std::shared_ptr<ZmqConnection>(&zmq_connection);
+
+    DeviceDescriptor desc;
+
+    try {
+        desc = acquire_device_description(connection);
+    } catch (std::runtime_error &e) {
+        printftostderr("main", "%s\n", e.what());
+        return -1;
+    }
 
     doocs::Server server("hub_server");
 
